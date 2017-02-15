3c4ec3ce5d38daace0431952e107f7efa789cf14
Rico <rico1310@live.com>
2017-02-15 14:33:35 +0100

    assignment 10

--- template/scripts/main.js
+++ template/scripts/main.js
@@ -1,1 +1,7 @@
-alert("It works!");
+var newFooter = document.createElement("footer");
+var newFooterParagraph = document.createElement("p");
+var newFooterParagraphText = document.createTextNode("copyright: I don't know media.");
+
+newFooterParagraph.appendChild(newFooterParagraphText);
+newFooter.appendChild(newFooterParagraph);
+document.body.appendChild(newFooter);
