ee1bb52efb3f6493fc7a19e41b541db35a212c47
Rico <rico1310@live.com>
2017-03-06 22:57:28 +0100

    added speed and started starlog

--- Assignments/Week2 Assignment/scripts/main.js
+++ Assignments/Week2 Assignment/scripts/main.js
@@ -1,19 +1,29 @@
-var currentSpeed = document.getElementById("currentSpeed").innerHTML;
-setInterval(runSpeed);
-function runSpeed(currentSpeed){
-    if(currentSpeed<7800){
-        alert("LOW");
-        currentSpeed += 300;
-        alert(currentSpeed);
+window.onload = function(){
+    runPage();
+};
+
+function runPage(){
+    var currentSpeedElement = document.getElementById("currentSpeed");
+    var currentSpeed = Number(currentSpeedElement.innerHTML);
+    var form = document.getElementById(form);
+
+    function randomRange(min, max){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    else if(currentSpeed>8000){
-        alert("HIGH");
-        currentSpeed -= 300;
-        alert(currentSpeed);
+
+    function runSpeed(){
+        if(currentSpeed<8000){
+            currentSpeed += randomRange(-50,200);
+            currentSpeedElement.innerHTML = currentSpeed;
+        }
+        else if(currentSpeed>12000){
+            currentSpeed += randomRange(-200,50);
+            currentSpeedElement.innerHTML = currentSpeed;
+        }
+        else {
+            currentSpeed += randomRange(-100,100);
+            currentSpeedElement.innerHTML = currentSpeed;
+        }
     }
-    else {
-        alert("MID");
-        currentSpeed -= 300;
-        alert(currentSpeed);
-    }
+    setInterval(runSpeed,100);
 }
