c10e8611f3d81629a72329f2be1d6b8a141c9939
Rico <rico1310@live.com>
2017-03-06 20:54:56 +0100

    tweaked number

--- Assignments/Week2 Assignment/scripts/main.js
+++ Assignments/Week2 Assignment/scripts/main.js
@@ -1,2 +1,19 @@
-var currentSpeed = document.getElementById("currentSpeed");
-alert(currentSpeed.innerHTML);
+var currentSpeed = document.getElementById("currentSpeed").innerHTML;
+setInterval(runSpeed);
+function runSpeed(currentSpeed){
+    if(currentSpeed<7800){
+        alert("LOW");
+        currentSpeed += 300;
+        alert(currentSpeed);
+    }
+    else if(currentSpeed>8000){
+        alert("HIGH");
+        currentSpeed -= 300;
+        alert(currentSpeed);
+    }
+    else {
+        alert("MID");
+        currentSpeed -= 300;
+        alert(currentSpeed);
+    }
+}
