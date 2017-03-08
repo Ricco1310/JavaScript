cace039307e159a8450f525fe2604a9aa97f5126
Rico <rico1310@live.com>
2017-03-08 23:00:08 +0100

    updated to contain all, broke clock running

--- Assignments/Week4 Assignment/scripts/main.js
+++ Assignments/Week4 Assignment/scripts/main.js
@@ -1,7 +1,21 @@
 var clockSetter = document.getElementById("clockSetter");
-var dateSetter = document.getElementById("dateSetter")
+var dateSetter = document.getElementById("dateSetter");
 
-function updateTime(){
+var clockSetterPreOut = document.getElementById("clockSetterPreOut");
+var dateSetterPreOut = document.getElementById("dateSetterPreOut");
+
+var clockSetterPre = document.getElementById("clockSetterPre");
+var dateSetterPre = document.getElementById("dateSetterPre");
+
+var clockSetterPast = document.getElementById("clockSetterPast");
+var dateSetterPast = document.getElementById("dateSetterPast");
+
+var clockSetterPastOut = document.getElementById("clockSetterPastOut");
+var dateSetterPastOut = document.getElementById("dateSetterPastOut");
+
+var t1 = new TimelineMax();
+
+function updateTime(elementToChangeDate, elementToChangeClock) {
     var utcTime = new Date();
     //day in month
     var Tdate = utcTime.getUTCDate();
@@ -20,12 +34,20 @@ function updateTime(){
     //give the day a name
     TdayName = dayNumberToName(Tday);
     //update html date
-    dateSetter.innerHTML = TdayName + " " + Tdate + " " + Tmonth + " " + TfullYear;
+
+    //dateSetter.innerHTML = TdayName + " " + Tdate + " " + Tmonth + " " + TfullYear;
     //update html time
-    clockSetter.innerHTML = Thours + " " + Tminutes + " " + Tseconds;
+    elementToChangeDate.children[0].innerHTML = TdayName;
+    elementToChangeDate.children[1].innerHTML = Tdate;
+    elementToChangeDate.children[2].innerHTML = Tmonth;
+    elementToChangeDate.children[3].innerHTML = TfullYear;
+    //clockSetter.innerHTML = Thours + " " + Tminutes + " " + Tseconds;
+    elementToChangeClock.children[0].innerHTML = Thours;
+    elementToChangeClock.children[1].innerHTML = Tminutes;
+    elementToChangeClock.children[2].innerHTML = Tseconds;
 }
 
-function dayNumberToName(Tday){
+function dayNumberToName(Tday) {
     switch (Tday) {
         case 0:
             return "Sunday";
@@ -50,4 +72,20 @@ function dayNumberToName(Tday){
     }
 }
 
-setInterval(updateTime, 1000);
+setInterval(updateTime(dateSetterPreOut, clockSetterPreOut), 100);
+setInterval(updateTime(dateSetterPre, clockSetterPre), 100);
+setInterval(updateTime(dateSetter, clockSetter), 100);
+setInterval(updateTime(dateSetterPast, clockSetterPast), 100);
+setInterval(updateTime(dateSetterPastOut, clockSetterPastOut), 100);
+//setInterval(changeColor, 8000);
+//changeColor();
+
+
+//start of animation coding
+
+function changeColor() {
+    t1.to("#clockSetter", 2, { color: "rgb(0,0,0)" })
+    t1.to("#clockSetter", 2, { color: "rgb(0,0,255)" })
+    t1.to("#clockSetter", 2, { color: "rgb(0,255,0)" })
+    t1.to("#clockSetter", 2, { color: "rgb(255,0,0)" })
+}
