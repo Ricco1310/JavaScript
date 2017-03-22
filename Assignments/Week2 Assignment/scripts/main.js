de378defac9d7bbedbe0214a7530020e7b0c0e91
Rico <rico1310@live.com>
2017-03-22 15:05:39 +0100

    nearly finished ass 1

--- Assignments/Week2 Assignment/scripts/main.js
+++ Assignments/Week2 Assignment/scripts/main.js
@@ -2,7 +2,7 @@ window.onload = function(){
     runPage();
 };
 
-function runPage(){
+function changingSpeed(){
     var currentSpeedElement = document.getElementById("currentSpeed");
     var currentSpeed = Number(currentSpeedElement.innerHTML);
     var form = document.getElementById(form);
@@ -26,4 +26,49 @@ function runPage(){
         }
     }
     setInterval(runSpeed,100);
+
+}
+
+function starLog(){
+    var starlog = document.getElementById("form");
+    var formName = document.getElementById("formName");
+    var formDate = document.getElementById("formDate");
+    var formMessage = document.getElementById("formMessage");
+    var latestName = document.getElementById("latestName");
+    var latestDate = document.getElementById("latestDate");
+    var latestMessage = document.getElementById("latestMessage");
+    starlog.onsubmit = function(){
+        alert(0);
+        latestName.innerHTML = formName.value;
+        latestDate.innerHTML = formDate.value;
+        latestMessage.innerHTML = formMessage.value;
+        formName.value = "";
+        formDate.value = "";
+        formMessage.value = "";
+        return false;
+    }
+}
+
+function mouseOverImage(){
+    var animating = false;
+    opacityImage = 1;
+    opacitytext = 0;
+    var landingImage = document.getElementById("landingImage");
+    var landingText = document.getElementById("landingText");
+    landingImage.onmouseenter = function(){
+        var landingImageAnimate
+        landingImage.style.opacity = 0.3;
+        landingText.style.opacity = 1;
+    }
+    landingImage.onmouseleave = function(){
+        var landingTextAnimate
+        landingImage.style.opacity = 1;
+        landingText.style.opacity = 0;
+    }
+}
+
+function runPage(){
+    changingSpeed();
+    starLog();
+    mouseOverImage();
 }
