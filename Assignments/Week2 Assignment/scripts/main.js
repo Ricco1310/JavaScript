9d8643faf30ea57c66125231bcc956279d39682f
Rico <rico1310@live.com>
2017-03-27 15:33:27 +0200

    pretty much finished ass 1

--- Assignments/Week2 Assignment/scripts/main.js
+++ Assignments/Week2 Assignment/scripts/main.js
@@ -2,16 +2,23 @@ window.onload = function(){
     runPage();
 };
 
-function changingSpeed(){
+function changingCounter(){
     var currentSpeedElement = document.getElementById("currentSpeed");
     var currentSpeed = Number(currentSpeedElement.innerHTML);
     var form = document.getElementById(form);
+    var currentDistanceElement = document.getElementById("currentDistance");
+    var currentDistance = Number(currentDistanceElement.innerHTML);
+    var currentFuelElement = document.getElementById("currentFuel");
+    var currentFuel = Number(currentFuelElement.innerHTML);
+    var currentDistancePart = 1242000000;
 
+    //Generates random number
     function randomRange(min, max){
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function runSpeed(){
+    function updateCounters(){
+        //randomizes the speed, tries to keep it around 10000
         if(currentSpeed<8000){
             currentSpeed += randomRange(-50,200);
             currentSpeedElement.innerHTML = currentSpeed;
@@ -24,9 +31,16 @@ function changingSpeed(){
             currentSpeed += randomRange(-100,100);
             currentSpeedElement.innerHTML = currentSpeed;
         }
+        //updates the distance based on speed
+        currentDistance += currentSpeed;
+        currentDistanceElement.innerHTML = currentDistance;
+        //update fuel
+        if (currentDistance > currentDistancePart){
+            currentFuel--;
+            currentDistancePart += currentDistancePart;
+        }
     }
-    setInterval(runSpeed,100);
-
+    setInterval(updateCounters,100);
 }
 
 function starLog(){
@@ -51,24 +65,47 @@ function starLog(){
 
 function mouseOverImage(){
     var animating = false;
-    opacityImage = 1;
-    opacitytext = 0;
+    var opacityImage = 1;
+    var opacitytext = 0;
     var landingImage = document.getElementById("landingImage");
     var landingText = document.getElementById("landingText");
-    landingImage.onmouseenter = function(){
-        var landingImageAnimate
-        landingImage.style.opacity = 0.3;
-        landingText.style.opacity = 1;
+
+    function changeOpacity(direction){
+        if (animating == false){
+            if (direction == true){
+                //show opacitytext
+                //animating = true until done
+                landingImage.style.opacity = 0.3;
+                landingText.style.opacity = 1;
+            }
+            else if(direction == false){
+                landingImage.style.opacity = 1;
+                landingText.style.opacity = 0;
+            }
+        }
     }
+
+    landingImage.onmouseenter = function(){
+        changeOpacity(true);
+    }
+    
     landingImage.onmouseleave = function(){
-        var landingTextAnimate
-        landingImage.style.opacity = 1;
-        landingText.style.opacity = 0;
+        changeOpacity(false);
     }
 }
 
+function getClock(){
+    var earthTime = document.getElementById("earthTime");
+    setInterval(function(){
+        var currentDate = new Date();
+        earthTime.innerHTML = currentDate.toUTCString();
+        },100
+    )
+}
+
 function runPage(){
-    changingSpeed();
+    changingCounter();
     starLog();
     mouseOverImage();
+    getClock();
 }
