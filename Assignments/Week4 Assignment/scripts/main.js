cddaec358068df25c704f20edb933ca7f2fa1997
Rico <rico1310@live.com>
2017-04-12 20:25:55 +0200

    added utc to lcoale onclick

--- Assignments/Week4 Assignment/scripts/main.js
+++ Assignments/Week4 Assignment/scripts/main.js
@@ -1,36 +1,41 @@
 var clockSetter = document.getElementById("clockSetter");
-var dateSetter = document.getElementById("dateSetter");
 var t1 = new TimelineMax();
 var t2 = new TimelineMax();
 
+var UTC = true;
+
 function updateTime(localTime, localDate, utcTime) {
     var dateDataArray = computeDateData(utcTime);
-    //update html date
-    //localDate.innerHTML = dateDataArray[7] + " " + dateDataArray[2] + "-" + dateDataArray[1] + "-" + dateDataArray[0];
-    //update html time
-    localTime.innerHTML = dateDataArray[4] + ":" + dateDataArray[5] + ":" + dateDataArray[6];
+    localTime.innerHTML = dateDataArray[0] + ":" + dateDataArray[1] + ":" + dateDataArray[2];
+}
+
+function changeUTCtoLocale(){
+    UTC = !UTC;
 }
 
 function computeDateData(utcTime) {
-    //array with all data going year to seconds
-    var dateDataArray = [
-        //0
-        utcTime.getUTCFullYear(),
-        //1
-        utcTime.getUTCMonth(),
-        //2
-        utcTime.getUTCDate(),
-        //3
-        utcTime.getUTCDay(),
-        //4
-        utcTime.getUTCHours(),
-        //5
-        utcTime.getUTCMinutes(),
-        //6
-        utcTime.getUTCSeconds(),
-        //7
-        dayNumberToName(utcTime.getUTCDay())
-    ];
+    if (UTC) {
+        //array with all data going year to seconds in utc format
+        var dateDataArray = [
+            //0
+            utcTime.getUTCHours(),
+            //1
+            utcTime.getUTCMinutes(),
+            //2
+            utcTime.getUTCSeconds()
+        ];
+    } else {
+        //array with all data going year to seconds in locale format
+        var dateDataArray = [
+            //0
+            utcTime.getHours(),
+            //1
+            utcTime.getMinutes(),
+            //2
+            utcTime.getSeconds()
+        ];
+    }
+
     for (var i = 0; i < dateDataArray.length; i++) {
         if (dateDataArray[i] < 10) {
             dateDataArray[i] = "0" + dateDataArray[i];
@@ -58,8 +63,10 @@ function animateClock(utcTime) {
     }
 }
 
+window.onclick = function () {
+    changeUTCtoLocale();
+}
 
-//setInterval(updateTime, 1000, clockSetter, dateSetter);
 window.onload = function () {
     setInterval(function () {
         var utcTime = new Date();
